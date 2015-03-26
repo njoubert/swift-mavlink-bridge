e47d5ed4f3d98dd7c5e6ea084021edc8cff15f44
Niels Joubert <njoubert@gmail.com>
2015-03-26 12:58:00 -0700

    Also returning the rest of the SBP-related data

--- dataflashBridge.py
+++ dataflashBridge.py
@@ -28,11 +28,18 @@ def extractSBP(filename):
 
     bin_data = None
     timestamp = None
+    msg_type = None
+    sender_id = None
+    msg_len = None
+
     if last_m != None and last_m.get_type() == 'SBR1' and m.get_type() == 'SBR2':
       #append the two
       bin_len = last_m.msg_len
       timestamp = getattr(last_m, '_timestamp', 0.0)
       bin_data = bytearray((last_m.d1 + m.d2 + m.d3 + m.d4)[0:bin_len])
+      msg_type = last_m.msg_type
+      sender_id = last_m.sender_id
+      msg_len = last_m.msg_len
 
 
     elif last_m != None and last_m.get_type() == 'SBR1' and m.get_type() == 'SBR1':
@@ -40,13 +47,17 @@ def extractSBP(filename):
       bin_len = last_m.msg_len
       timestamp = getattr(last_m, '_timestamp', 0.0)
       bin_data = bytearray((last_m.d1)[0:bin_len])
+      msg_type = last_m.msg_type
+      sender_id = last_m.sender_id
+      msg_len = last_m.msg_len
       last_m = m
+
     else:
       #just save this one
       last_m = m
 
     if bin_data != None:
-      extractedData.append((timestamp, bin_data))
+      extractedData.append((timestamp, msg_type, sender_id, msg_len, bin_data))
   return extractedData
     
 
